a98e30b8298133a18eb1ac104b278b7f21380429
boodyshlby55 <boodyshlby55@gmail.com>
2024-09-08 13:18:39 +0300

    forget password & reviews

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,17 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { authGuard } from './guards/auth.guard';
 import { ProductsComponent } from './products/products.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', canActivate: [authGuard], component: HomeComponent },
-  { path: 'products', canActivate: [authGuard], component: ProductsComponent },
-  { path: 'products/:id', canActivate: [authGuard], component: ProductDetailsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'myReviews', canActivate: [authGuard], component: ReviewsComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'forgetPassword', component: ForgetPasswordComponent },
   { path: '**', component: NotFoundComponent }
 ];
